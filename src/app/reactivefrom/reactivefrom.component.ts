53d99620a1271270906e2d7e323d3698cdb835b6
Thaneshwara848 <thaneshwaramanjunatha@gmail.com>
2022-12-27 16:16:19 +0530

    validaton Demo

--- src/app/reactivefrom/reactivefrom.component.ts
+++ src/app/reactivefrom/reactivefrom.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactivefrom',
   templateUrl: './reactivefrom.component.html',
   styleUrls: ['./reactivefrom.component.css']
 })
-export class ReactivefromComponent {
+export class ReactivefromComponent implements OnInit {
+  student:any;
+  ngOnInit(): void {
 
+    this.student=new FormGroup({
+
+      username: new FormControl("",Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(10),
+        Validators.pattern("^[a-zA-z]*$")
+      ]))
+      ,
+      college: new FormControl("",Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(10)
+      ]))
+      ,
+      age: new FormControl("",Validators.compose([
+        Validators.required,
+        Validators.min(18),
+        Validators.max(30)
+      ]))
+      
+
+    })
+   
+  }
+  
 }
